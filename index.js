041c5c0940aa872925ce2b8f535064de8562709d
HansUrpay <hansurpayh@gmail.com>
2023-02-03 21:33:30 -0500

    prueba_3

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (_req, res) => {
+    res.send("Hola mundo por fin desplegó el proyecto")
+})
+
 app.listen(port);
 
 console.log(`Listen in port ${port}`);
